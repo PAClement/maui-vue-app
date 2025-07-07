0b7e36159f413b53b6b63d60e38437dbc3541ce2
PAClement <clementpaquentin@hotmail.fr>
2025-07-07 13:41:02 +0200

    feat(component): add disabled prop to Button component for better accessibility

--- src/tools/Button.vue
+++ src/tools/Button.vue
@@ -1,6 +1,7 @@
 <template>
   <button
-      :class="[ props.buttonClass ? props.buttonClass : 'bg-white shadow-md rounded-lg px-4 py-2 text-lg font-bold text-gray-500' ,'cursor-pointer']">
+      :disabled="props.disabled"
+      :class="[ props.buttonClass ? props.buttonClass : 'bg-white shadow-md rounded-lg px-4 py-2 text-lg font-bold text-gray-500' ,props.disabled ? '': 'cursor-pointer']">
     <span :class="['flex items-center gap-8', props.iconSide === 'right' ? 'flex-row-reverse' : '']">
       <font-awesome-icon :icon="props.icon" :class="[props.iconColor, 'text-2xl']" v-if="props.icon"/>
       {{ props.text }}
@@ -15,5 +16,9 @@ const props = defineProps({
   iconSide: String,
   icon: String,
   iconColor: String,
+  disabled: {
+    type: Boolean,
+    default: false
+  }
 })
 </script>
