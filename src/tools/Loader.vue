393c0f70db82b9c2d19737037664246ad25d3c12
PAClement <clementpaquentin@hotmail.fr>
2025-07-08 01:29:49 +0200

    feat(tools): add Loader component for loading indication

---
+++ src/tools/Loader.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex items-center justify-center h-32">
+    <div class="w-12 h-12 border-4 border-primary-500 border-t-transparent rounded-full animate-spin"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
