4f406df37cdb73fb043366284b5ca12bddde3ee8
PAClement <clementpaquentin@hotmail.fr>
2025-07-07 10:06:00 +0200

    feat(component): add button component

---
+++ src/tools/Button.vue
@@ -0,0 +1,19 @@
+<template>
+  <button
+      :class="[ props.buttonClass ? props.buttonClass : 'bg-white shadow-md rounded-lg px-4 py-2 text-lg font-bold text-gray-500' ,'cursor-pointer']">
+    <span :class="['flex items-center gap-8', props.iconSide === 'right' ? 'flex-row-reverse' : '']">
+      <font-awesome-icon :icon="props.icon" :class="[props.iconColor, 'text-2xl']" v-if="props.icon"/>
+      {{ props.text }}
+    </span>
+  </button>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  text: String,
+  buttonClass: String,
+  iconSide: String,
+  icon: String,
+  iconColor: String,
+})
+</script>
