4679369740da3931bcf1769c6f1661745432a172
PAClement <clementpaquentin@hotmail.fr>
2025-07-07 13:41:22 +0200

    feat(component): add Cart and Loyalty components

---
+++ src/components/StepSideCheckout/Loyalty.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex flex-col items-center justify-between h-full py-20">
+    <h2 class="text-2xl font-bold text-gray-500">Vous avez un compte de fidélité ?</h2>
+    <div class="text-center">
+     button
+    </div>
+    <Button  @click="$emit('next')" text="Non, merci" buttonClass="bg-gray-500 shadow-md rounded-lg flex items-center
+            gap-16 px-12 py-4 font-bold text-white text-2xl cursor-pointer " iconSide="left"
+             icon="ban" iconColor="text-white"/>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import Button from "../../tools/Button.vue";
+</script>
