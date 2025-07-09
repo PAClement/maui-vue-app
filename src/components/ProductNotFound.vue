f621081692b2aa14d5981108c0d6f8b8d1e38bfd
PAClement <clementpaquentin@hotmail.fr>
2025-07-09 12:20:05 +0200

    feat(component): add ProductNotFound modal for user feedback

---
+++ src/components/ProductNotFound.vue
@@ -0,0 +1,30 @@
+<template>
+  <TransitionRoot appear :show="displayModal" as="template">
+    <Dialog as="div" class="relative z-50" @close="$emit('close-modal')">
+      <div class="fixed inset-0 bg-black/30 backdrop-blur-sm"/>
+      <div class="fixed inset-0 flex items-center justify-center p-4">
+        <DialogPanel
+            class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left shadow-xl transition-all">
+          <div class="flex flex-col items-center justify-center gap-5">
+            <font-awesome-icon icon="warning" class="text-6xl text-yellow-500"/>
+            <h2 class="text-gray-500 text-center">
+              <span class="font-bold text-lg">Produit non trouvé</span>
+              <br><br>
+              <span class="text-md">Un agent va venir vous aider</span>
+            </h2>
+          </div>
+        </DialogPanel>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  displayModal: {
+    type: Boolean,
+    default: false
+  }
+})
+import {Dialog, DialogPanel, TransitionRoot} from "@headlessui/vue";
+</script>
