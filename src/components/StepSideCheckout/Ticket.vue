7f49208f36a31c5254e096431e2229c17ab9d8cf
PAClement <clementpaquentin@hotmail.fr>
2025-07-09 11:47:57 +0200

    fix(component): change icon alignment in ticket delivery buttons

--- src/components/StepSideCheckout/Ticket.vue
+++ src/components/StepSideCheckout/Ticket.vue
@@ -3,13 +3,13 @@
     <h2 class="text-2xl font-bold text-gray-500">Voulez-vous un ticket ?</h2>
     <div class="flex flex-col items-stretch gap-5 ">
       <Button @click="modalMail = true" text="Par mail" buttonClass="bg-primary-500 shadow-md rounded-lg flex items-center
-              gap-16 px-12 py-4 font-bold text-white text-xl cursor-pointer " iconSide="left"
+              gap-16 px-12 py-4 font-bold text-white text-xl cursor-pointer " iconSide="bottom"
               icon="envelope" iconColor="text-white"/>
       <Button @click="modalSMS = true" text="Par SMS" buttonClass="bg-primary-500 shadow-md rounded-lg flex items-center
-              gap-16 px-12 py-4 font-bold text-white text-xl cursor-pointer " iconSide="left"
+              gap-16 px-12 py-4 font-bold text-white text-xl cursor-pointer " iconSide="bottom"
               icon="mobile" iconColor="text-white"/>
       <Button @click="modalQRCode = true" text="QR Code" buttonClass="bg-primary-500 shadow-md rounded-lg flex items-center
-              gap-16 px-12 py-4 font-bold text-white text-xl cursor-pointer " iconSide="left"
+              gap-16 px-12 py-4 font-bold text-white text-xl cursor-pointer " iconSide="bottom"
               icon="qrcode" iconColor="text-white"/>
     </div>
     <Button @click="$emit('next')" text="Je ne veux pas de ticket" buttonClass="bg-gray-500 shadow-md rounded-lg flex items-center
