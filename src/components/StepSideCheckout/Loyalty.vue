3ba574e64e7a0d3828f90ae136954002ba726ca3
PAClement <clementpaquentin@hotmail.fr>
2025-07-09 17:38:31 +0200

    feat(component): remove unnecessary whitespace in Loyalty.vue for cleaner code

--- src/components/StepSideCheckout/Loyalty.vue
+++ src/components/StepSideCheckout/Loyalty.vue
@@ -2,7 +2,6 @@
   <div class="flex flex-col items-center justify-between h-full py-20">
     <h2 class="text-2xl font-bold text-gray-500">Vous avez un compte de fidélité ?</h2>
     <img src="@/assets/img/card-box.svg" alt="logo" class="w-30 h-30">
-
     <div>
       <Button @click="modalLoyaltyAccount = true" text="Associer mon compte" buttonClass="bg-primary-500 shadow-md rounded-lg flex items-center
             gap-16 px-12 py-4 font-bold text-white text-xl cursor-pointer " iconSide="left"
