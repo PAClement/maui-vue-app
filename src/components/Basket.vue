83937a8ea8f325700247776f3ca5246f953ac540
PAClement <clementpaquentin@hotmail.fr>
2025-07-09 11:08:26 +0200

    feat(component): add Basket component to display cart items and total amount

---
+++ src/components/Basket.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="flex-[1]">
+    <div class="flex items-center justify-between">
+      <span class="text-3xl font-bold text-gray-500">Votre Panier</span>
+      <span class="text-lg font-bold text-gray-500">{{
+          store.customerBasketInformation.totalBasketQuantity
+        }} ARTICLES</span>
+    </div>
+    <hr class="h-0.5 mt-3 mb-5 bg-gray-400 border-0 rounded">
+  </div>
+  <div class="flex-[9]">
+    <div class="flex flex-col gap-3">
+      <div v-for="product in store.products">
+        <Article :product="product.Name" :refProduct="product.InternalRef ?? ''" :price="product.Price"
+                 :displayButton="currentStep === 'cart'"/>
+      </div>
+    </div>
+  </div>
+  <div class="flex-[1]">
+    <hr class="h-0.5 mt-3 mb-5 bg-gray-400 border-0 rounded">
+    <div class="flex items-center justify-between">
+      <span class="text-3xl font-bold text-gray-500">Total</span>
+      <span
+          class="text-lg font-bold text-gray-500">{{
+          store.customerBasketInformation.totalBasketAmount?.toFixed(2)
+        }} €</span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  currentStep: string;
+}>();
+import Article from "@/components/Article.vue";
+import {useBlazorStore} from "@/plugins/blazorEvent";
+
+const store = useBlazorStore();
+</script>
