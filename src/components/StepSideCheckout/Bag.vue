a83546dd4cecf54e1def496a7bd5cdc9bd2712df
PAClement <clementpaquentin@hotmail.fr>
2025-07-09 15:09:37 +0200

    fix(component): correct French text for scanning instruction

--- src/components/StepSideCheckout/Bag.vue
+++ src/components/StepSideCheckout/Bag.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col items-center justify-between h-full py-20">
     <h2 class="text-2xl font-bold text-gray-500">Voulez-vous un sac ?</h2>
     <div class="text-center">
-      <small class="text-gray-500 text-lg">Scanner le</small>
+      <small class="text-gray-500 text-lg">Scannez le</small>
       <img src="@/assets/img/barcode.svg" alt="logo" class="w-40 h-40">
     </div>
     <Button  @click="$emit('next')" text="Non, merci" buttonClass="bg-gray-500 shadow-md rounded-lg flex items-center
