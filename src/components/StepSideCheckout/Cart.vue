6c5bafea87feab348ccb27b91b3a869ac856044b
PAClement <clementpaquentin@hotmail.fr>
2025-07-09 11:11:04 +0200

    feat(component): enhance Cart component with a new layout and barcode image

--- src/components/StepSideCheckout/Cart.vue
+++ src/components/StepSideCheckout/Cart.vue
@@ -1,5 +1,8 @@
 <template>
-
+  <div class="flex flex-col items-center justify-center h-full py-20 gap-20">
+    <h2 class="text-2xl font-bold text-gray-500">Scanner vos articles</h2>
+    <img src="@/assets/img/barcode.svg" alt="logo" class="w-40 h-40">
+  </div>
 </template>
 
 <script setup lang="ts">
