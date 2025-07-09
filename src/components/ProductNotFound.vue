2e875df818987ada22b2f87a597277cf74e1864e
PAClement <clementpaquentin@hotmail.fr>
2025-07-09 14:48:02 +0200

    feat(component): update ProductNotFound modal to include retry button and improve close functionality

--- src/components/ProductNotFound.vue
+++ src/components/ProductNotFound.vue
@@ -1,6 +1,6 @@
 <template>
   <TransitionRoot appear :show="displayModal" as="template">
-    <Dialog as="div" class="relative z-50" @close="$emit('close-modal')">
+    <Dialog as="div" class="relative z-50" @close="closeModal">
       <div class="fixed inset-0 bg-black/30 backdrop-blur-sm"/>
       <div class="fixed inset-0 flex items-center justify-center p-4">
         <DialogPanel
@@ -9,9 +9,10 @@
             <font-awesome-icon icon="warning" class="text-6xl text-yellow-500"/>
             <h2 class="text-gray-500 text-center">
               <span class="font-bold text-lg">Produit non trouvé</span>
-              <br><br>
-              <span class="text-md">Un agent va venir vous aider</span>
             </h2>
+            <div>
+              <Button @click="closeModal" text="Réessayer"/>
+            </div>
           </div>
         </DialogPanel>
       </div>
@@ -20,11 +21,22 @@
 </template>
 
 <script setup lang="ts">
+import Button from "@/tools/Button.vue";
+import {Dialog, DialogPanel, TransitionRoot} from "@headlessui/vue";
+import {useBlazorStore} from "@/plugins/blazorEvent";
+
 defineProps({
   displayModal: {
     type: Boolean,
     default: false
   }
-})
-import {Dialog, DialogPanel, TransitionRoot} from "@headlessui/vue";
+});
+
+const emit = defineEmits(['close-modal']);
+const store = useBlazorStore()
+
+const closeModal = () => {
+  store.productNotFound = false;
+  emit("close-modal");
+}
 </script>
